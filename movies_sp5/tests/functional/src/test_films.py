3824dd195975715e32be4ed869f564841a5624dc
Aleksandr Parfentev <parfen.a.v@gmail.com>
2023-05-23 14:59:18 +0300

    Adds positive tests for movies

---
+++ movies_sp5/tests/functional/src/test_films.py
@@ -0,0 +1,80 @@
+import random
+import pytest
+
+from ..testdata.films import random_films
+
+
+class TestFilms:
+    @pytest.mark.parametrize(
+        'query_data, order',
+        [
+            ({'sort': '-imdb_rating'}, 'desc'),
+            ({'sort': 'imdb_rating'}, 'asc'),
+        ] 
+    )
+    @pytest.mark.asyncio
+    async def test_films_sorted(self, es_write_data, make_get_request, query_data, order):
+        films = random_films(qty=3)
+
+        await es_write_data(films, 'movies', 'id')
+
+        body = await make_get_request('/films', query_data)
+
+        assert len(body['items']) == 3
+        assert body['items'] == [
+            {'uuid': film['id'], 'title': film['title'], 'imdb_rating': film['imdb_rating']}
+            for film
+            in sorted(films, key=lambda film: film['imdb_rating'], reverse=order=='desc')
+        ]
+
+    @pytest.mark.asyncio
+    @pytest.mark.skip('Не работает фильтрация по жанрам')
+    async def test_films_filtered(self, es_write_data, make_get_request):
+        films = random_films(qty=3)
+        film_for_filtering = random.choice(films)
+
+        await es_write_data(films, 'movies', 'id')
+
+        body = await make_get_request('/films', {'sort': '-imdb_rating', 'genre': random.choice(film_for_filtering['genres'])['id']})
+
+        assert body['items'] == [{'uuid': film_for_filtering['id'], 'title': film_for_filtering['title'], 'imdb_rating': film_for_filtering['imdb_rating']}]
+
+    @pytest.mark.asyncio
+    async def test_film_by_id(self, es_write_data, make_get_request):
+        films = random_films(qty=3)
+        film_for_get = random.choice(films)
+
+        await es_write_data(films, 'movies', 'id')
+
+        body = await make_get_request(f'/films/{film_for_get["id"]}')
+
+        assert body == {
+            'uuid': film_for_get['id'],
+            'title': film_for_get['title'],
+            'imdb_rating': film_for_get['imdb_rating'],
+            'description': film_for_get['description'],
+            'genre': [
+                {
+                    'uuid': genre['id'],
+                    'name': genre['name']
+                } for genre in film_for_get['genres']
+            ],
+            'actors': [
+                {
+                    'uuid': actor['id'],
+                    'full_name': actor['name']
+                } for actor in film_for_get['actors']
+            ],
+            'writers': [
+                {
+                    'uuid': writer['id'],
+                    'full_name': writer['name']
+                } for writer in film_for_get['writers']
+            ],
+            'directors': [
+                {
+                    'uuid': director['id'],
+                    'full_name': director['name']
+                } for director in film_for_get['directors']
+            ],
+        }
--- movies_sp5/tests/functional/src/test_search.py
+++ movies_sp5/tests/functional/src/test_search.py
@@ -2,23 +2,16 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    'query_data, expected_answer',
+    'query_data, expected_count',
     [
-        (
-                {'query': 'The Star'},
-                {'status': 200, 'length': 10}
-        ),
-        (
-                {'query': 'Mashed potato'},
-                {'status': 200, 'length': 0}
-        )
+        ({'query': 'The Star'}, 10),
+        ({'query': 'Mashed potato'}, 0),
     ]
 )
 @pytest.mark.asyncio
-async def test_search(query_data, expected_answer, es_data, es_write_data, make_get_request):   
-    await es_write_data(es_data, 'movies', 'id') 
+async def test_search(query_data, expected_count, es_data, es_write_data, make_get_request):   
+    await es_write_data(es_data, 'movies', 'id')
 
-    status, body = await make_get_request('/films/search', query_data)
+    body = await make_get_request('/films/search', query_data)
 
-    assert status == expected_answer['status']
-    assert len(body['items']) == expected_answer['length'], body
+    assert len(body['items']) == expected_count, body
