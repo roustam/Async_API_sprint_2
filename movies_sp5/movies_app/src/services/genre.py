6d18f3b6376598abda6e5649e044829a27886df0
Aleksandr Parfentev <parfen.a.v@gmail.com>
2023-05-27 22:41:32 +0300

    Refactors services

--- movies_sp5/movies_app/src/services/genre.py
+++ movies_sp5/movies_app/src/services/genre.py
@@ -1,113 +1,56 @@
-import json
 from functools import lru_cache
 
 from elasticsearch import AsyncElasticsearch, NotFoundError
 from fastapi import Depends
+import orjson
 from redis.asyncio import Redis
 
 from db.elastic import get_elastic
 from db.redis import get_redis
 from models.genre import Genre
-from services.common import make_redis_key, CACHE_EXPIRE_IN_SECONDS
+from services.common import make_redis_key, CACHE_EXPIRE_IN_SECONDS, ServiceAbstract
 
 
-class GenreService:
-    def __init__(self, redis: Redis, elastic: AsyncElasticsearch):
-        self.redis = redis
-        self.elastic = elastic
-
-    async def get(self, page: int, size: int) -> tuple[list[Genre], int]:
-        genres_total = await self._get_genres_from_cache(page, size)
-
-        if genres_total is None:
-            genres_total = await self._get_genres_from_elastic(page, size)
-
-            if genres_total is None:
-                return [], 0
-
-        genres, total = genres_total
-        response = (genres, total)
-        await self._put_genres_to_cache(response, page, size)
-
-        return response
-
-    async def _get_genres_from_cache(
-        self, page: int, size: int
-    ) -> tuple[list[Genre], int] | None:
-        cache_key = make_redis_key('genres', page, size)
-        cached_genres = await self.redis.get(cache_key)
-
-        if not cached_genres:
-            return None
-
-        data = json.loads(cached_genres)
-        genres_data = data["genres"]
-        genres = [Genre(id=genre["id"], name=genre["name"]) for genre in genres_data]
-        total = data["count"]
-
-        return genres, total
-
-    async def _get_genres_from_elastic(
-        self, page: int, size: int
-    ) -> tuple[list[Genre], int]:
-        es_query = {
-            "query": {"match_all": {}},
-            "sort": [{"name.raw": "asc"}],
-            "from": (page - 1) * size,
-            "size": size,
-        }
-        result = await self.elastic.search(index="genres", body=es_query)
-        total = result["hits"]["total"]["value"]
-
-        genres = [
-            Genre(id=genre["_id"], name=genre["_source"]["name"])
-            for genre in result["hits"]["hits"]
-        ]
-
-        return genres, total
-
-    async def _put_genres_to_cache(
-        self, response: tuple[list[Genre], int], page: int, size: int
-    ) -> None:
-        cache_key = make_redis_key('genres', page, size)
-        genres, total = response
-        genres_data = [{"id": genre.id, "name": genre.name} for genre in genres]
-        data = {"genres": genres_data, "count": total}
-        await self.redis.set(cache_key, json.dumps(data), ex=CACHE_EXPIRE_IN_SECONDS)
-
+class GenreService(ServiceAbstract):
     async def get_by_id(self, genre_id: str) -> Genre | None:
-        genre = await self._get_genre_from_cache(genre_id)
-
+        key = make_redis_key('genre', genre_id)
+        genre = await self._get_genres_from_cache(key)
         if not genre:
-            genre = await self._get_genre_from_elastic_by_id(genre_id)
+            genre = await self._get_genre_from_elastic(genre_id)
+            await self._save_genres_to_cache(key, genre)
+        return Genre(**genre['_source']) if genre else None
+    
+    async def get(self, page_number: int | None = 1, page_size: int | None = 10) -> list[Genre]:
+        key = make_redis_key('genres', page_number, page_size)
+        genres = await self._get_genres_from_cache(key)
+        if not genres:
+            genres = await self._get_genres_from_elastic(page_number=page_number, page_size=page_size)
+            await self._save_genres_to_cache(key, genres)
+        return [Genre(**doc['_source']) for doc in genres['hits']['hits']]
 
-            if not genre:
-                return None
+    async def _get_genres_from_cache(self, key) -> dict | list[dict]:
+        genres = await self.redis.get(key)
+        return orjson.loads(genres) if genres else None
+        
+    async def _save_genres_to_cache(self, key, genres):
+        await self.redis.set(key, orjson.dumps(genres).decode(), CACHE_EXPIRE_IN_SECONDS)
 
-            await self._put_genre_to_cache(genre)
-
-        return genre
-
-
-    async def _get_genre_from_elastic_by_id(self, genre_id: str) -> Genre | None:
+    async def _get_genre_from_elastic(self, genre_id: str) -> dict | None:
         try:
-            doc = await self.elastic.get("genres", genre_id)
+            return await self.elastic.get('genres', genre_id)
         except NotFoundError:
             return None
-        return Genre(**doc["_source"])
 
-    async def _get_genre_from_cache(self, genre_id: str) -> Genre | None:
-        cache_key = make_redis_key('genre', genre_id)
-        cashed_genre = await self.redis.get(cache_key)
-
-        if not cashed_genre:
-            return None
-
-        return Genre.parse_raw(cashed_genre)
-
-    async def _put_genre_to_cache(self, genre: Genre):
-        cache_key = make_redis_key('genre', genre.id)
-        await self.redis.set(cache_key, genre.json(), ex=CACHE_EXPIRE_IN_SECONDS)
+    async def _get_genres_from_elastic(
+        self, page_number: int | None = 1, page_size: int | None = 10
+    ) -> dict:
+        body = {
+            'query': {'match_all': {}},
+            'sort': [{'name.raw': 'asc'}],
+            'from': (page_number - 1) * page_size,
+            'size': page_size,
+        }
+        return await self.elastic.search(index='genres', body=body)
 
 
 @lru_cache()
--- movies_sp5/movies_app/src/services/person.py
+++ movies_sp5/movies_app/src/services/person.py
@@ -1,4 +1,3 @@
-import json
 from functools import lru_cache
 
 import orjson
@@ -8,200 +7,97 @@ from redis.asyncio import Redis
 
 from db.elastic import get_elastic
 from db.redis import get_redis
-from models.person import PersonFilm, PersonFilmDetails, \
-    PersonFilmDetailsRoles
-from services.common import make_redis_key, CACHE_EXPIRE_IN_SECONDS
+from models.person import Person, PersonFilmDetails
+from services.common import make_redis_key, CACHE_EXPIRE_IN_SECONDS, ServiceAbstract
 
 
-class PersonService:
-    def __init__(self, redis: Redis, elastic: AsyncElasticsearch):
-        self.redis = redis
-        self.elastic = elastic
-
-    async def get_by_id(self, person_id: str) -> PersonFilm | None:
-        person = await self._person_from_cache(person_id)
-
+class PersonService(ServiceAbstract):
+    async def get_by_id(self, person_id: str) -> Person | None:
+        key = make_redis_key('person', person_id)
+        person = await self._get_persons_from_cache(key)
         if not person:
-            person = await self._get_person_from_elastic_by_id(person_id)
+            person = await self._get_person_from_elastic(person_id)
+            await self._save_persons_to_cache(key, person)
+        return Person(**person['_source']) if person else None
+    
+    async def get_films_by_person_id(self, person_id: str, page_number: int | None = 1, page_size: int | None = 10) -> list[Person]:
+        key = make_redis_key('person_films', person_id)
+        person_films = await self._get_persons_from_cache(key)
+        if not person_films:
+            person_films = await self._search_person_films_in_elastic(person_id, page_number=page_number, page_size=page_size)
+            await self._save_persons_to_cache(key, person_films)
+        return [PersonFilmDetails(**doc['_source']) for doc in person_films['hits']['hits']]
+    
+    async def search(self, query: str, page_number: int | None = 1, page_size: int | None = 10) -> list[Person]:
+        key = make_redis_key('persons', query, page_number, page_size)
+        persons = await self._get_persons_from_cache(key)
+        if not persons:
+            persons = await self._search_persons_in_elastic(query, page_number=page_number, page_size=page_size)
+            await self._save_persons_to_cache(key, persons)
+        return [Person(**doc['_source']) for doc in persons['hits']['hits']]
 
-            if not person:
-                return None
-            await self._put_person_to_cache(person)
+    async def _get_persons_from_cache(self, key) -> dict | list[dict]:
+        persons = await self.redis.get(key)
+        return orjson.loads(persons) if persons else None
+        
+    async def _save_persons_to_cache(self, key, persons):
+        await self.redis.set(key, orjson.dumps(persons).decode(), CACHE_EXPIRE_IN_SECONDS)
 
-        return person
-
-    async def _get_person_from_elastic_by_id(
-        self, person_id: str
-    ) -> PersonFilm | None:
+    async def _get_person_from_elastic(self, person_id: str) -> dict | None:
         try:
-            doc = await self.elastic.get("persons", str(person_id))
-        except NotFoundError:
-            return None
-        return PersonFilm(**doc["_source"])
-
-    async def _person_from_cache(self, person_id: str) -> PersonFilm | None:
-        cache_key = make_redis_key('person', person_id)
-        data = await self.redis.get(cache_key)
-        if not data:
-            return None
-
-        return PersonFilm.parse_raw(data)
-
-    async def _put_person_to_cache(self, person: PersonFilm):
-        cache_key = make_redis_key('person', person.id)
-        await self.redis.set(cache_key, person.json(),
-                             person_CACHE_EXPIRE_IN_SECONDS)
-
-    async def _get_film_for_person_from_cache(self, key):
-        data = await self.redis.get(key)
-        if data:
-            return orjson.loads(data)
-        else:
-            return None
-
-    async def _set_film_for_person_to_cache(self, key, data):
-        data_json = orjson.dumps(data).decode()
-        await self.redis.set(key, data_json, person_CACHE_EXPIRE_IN_SECONDS)
-
-    async def _get_film_for_person_from_elastic(self, person_id) -> dict:
-        try:
-            result = await self.elastic.search(
-                {
-                    "query": {
-                        "bool": {
-                            "should": [
-                                {
-                                    "nested": {
-                                        "path": "actors",
-                                        "query": {
-                                            "term": {"actors.id": person_id}},
-                                    }
-                                },
-                                {
-                                    "nested": {
-                                        "path": "writers",
-                                        "query": {
-                                            "term": {"writers.id": person_id}},
-                                    },
-                                },
-                                {
-                                    "nested": {
-                                        "path": "directors",
-                                        "query": {"term": {
-                                            "directors.id": person_id}},
-                                    }
-                                },
-                            ]
-                        }
-                    }
-                },
-                "movies",
-            )
+            return await self.elastic.get('persons', person_id)
         except NotFoundError:
             return None
 
-        return result
-
-    async def get_films_by_person_id(self, person_id: str):
-        redis_key = make_redis_key('person_details', person_id)
-        res = await self._get_film_for_person_from_cache(redis_key)
-
-        if not res:
-            res = await self._get_film_for_person_from_elastic(person_id)
-            await self._set_film_for_person_to_cache(redis_key, res)
-
-        return [PersonFilmDetails(**doc["_source"]) for doc in
-                res["hits"]["hits"]]
-
-    async def _search_films_in_elastic(self, query: str) -> list:
-        res = await self.elastic.search(
-            {
-                "query": {
-                    "multi_match": {
-                        "query": query,
-                        "fuzziness": "auto",
-                        "fields": ["id", "full_name", "movies"]
+    async def _search_person_films_in_elastic(self, person_id, page_number: int | None = 1, page_size: int | None = 10) -> dict:
+        return await self.elastic.search(
+            index='movies',
+            body={
+                'query': {
+                    'bool': {
+                        'should': [
+                            {
+                                'nested': {
+                                    'path': 'actors',
+                                    'query': {
+                                        'term': {'actors.id': person_id}},
+                                }
+                            },
+                            {
+                                'nested': {
+                                    'path': 'writers',
+                                    'query': {
+                                        'term': {'writers.id': person_id}},
+                                },
+                            },
+                            {
+                                'nested': {
+                                    'path': 'directors',
+                                    'query': {'term': {
+                                        'directors.id': person_id}},
+                                }
+                            },
+                        ]
                     }
                 },
+                'from': (page_number - 1) * page_size,
+                'size': page_size,
             },
-            "persons",
         )
 
-        return res
-
-    async def search(self, query: str, page: int, size: int) \
-            -> list[PersonFilm]:
-        persons_with_films = await self._get_persons_with_films_from_cache(
-            query, page, size
-        )
-
-        if persons_with_films is None:
-            persons_with_films = await self._get_persons_with_films_from_elastic(
-                query, page, size)
-
-            if persons_with_films is None:
-                return []
-
-        await self._put_persons_with_films_to_cache(persons_with_films,
-                                                    query, page, size)
-
-        return persons_with_films
-
-    async def _get_persons_with_films_from_cache(
-        self, query: str, page: int, size: int
-    ) -> list[PersonFilm] | None:
-        cache_key = make_redis_key('persons', query, page, size)
-        cached_persons_with_films = await self.redis.get(cache_key)
-
-        if not cached_persons_with_films:
-            return None
-
-        deserialized_persons = json.loads(cached_persons_with_films)
-        persons_with_films = [PersonFilm(**person) for person in
-                              deserialized_persons]
-        return persons_with_films
-
-    async def _get_persons_with_films_from_elastic(self, query: str, page: int,
-                                                   size: int) \
-            -> list[PersonFilm]:
-        es_query = {
-            "query": {
-                "match": {
-                    "full_name": query
-                }
+    async def _search_persons_in_elastic(self, query: str, page_number: int | None = 1, page_size: int | None = 10) -> dict:
+        return await self.elastic.search(
+            index='persons',
+            body={
+                'query': {
+                    'match': {
+                        'full_name': query
+                    }
+                },
+                'from': (page_number - 1) * page_size,
+                'size': page_size,
             },
-            "from": (page - 1) * size,
-            "size": size,
-        }
-
-        result = await self.elastic.search(index="persons", body=es_query)
-
-        persons_with_films = []
-        for hit in result["hits"]["hits"]:
-            person = hit["_source"]
-            films = []
-            for movie in person["movies"]:
-                film = PersonFilmDetailsRoles(id=movie["id"],
-                                              roles=movie["roles"])
-                films.append(film)
-
-            person_film = PersonFilm(id=person["id"],
-                                     full_name=person["full_name"],
-                                     movies=films)
-            persons_with_films.append(person_film)
-        return persons_with_films
-
-    async def _put_persons_with_films_to_cache(
-        self,
-            persons_with_films: list[PersonFilm],
-            query: str,
-            page: int,
-            size: int
-    ) -> None:
-        cache_key = make_redis_key('persons', query, page, size)
-        serialized_persons = [person.dict() for person in persons_with_films]
-        await self.redis.set(cache_key, json.dumps(serialized_persons),
-                             ex=person_CACHE_EXPIRE_IN_SECONDS)
+        )
 
 
 @lru_cache()
