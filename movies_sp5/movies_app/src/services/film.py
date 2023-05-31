0fc1f4c9bb9bc2d615764fe42043144bad6a655e
Aleksandr Parfentev <parfen.a.v@gmail.com>
2023-05-31 21:35:57 +0300

    Refactos es and redis in app

--- movies_sp5/movies_app/src/services/film.py
+++ movies_sp5/movies_app/src/services/film.py
@@ -1,53 +1,39 @@
 from functools import lru_cache
 
-import orjson
-from elasticsearch import AsyncElasticsearch, NotFoundError
 from fastapi import Depends
-from redis.asyncio import Redis
 
 from db.elastic import get_elastic
 from db.redis import get_redis
 from models.film import Film
-from services.common import make_redis_key, CACHE_EXPIRE_IN_SECONDS, ServiceAbstract
+from db.elastic import ElasticClient
+from db.redis import RedisClient
+from services.common import make_redis_key, ServiceAbstract
 
 
 class FilmService(ServiceAbstract):
     async def get_by_id(self, film_id: str) -> Film | None:
         key = make_redis_key('film', film_id)
-        film = await self._get_films_from_cache(key)
+        film = await self.redis.get_by_key(key)
         if not film:
-            film = await self._get_film_from_elastic(film_id)
-            await self._save_films_to_cache(key, film)
+            film = await self.elastic.get_by_id('movies', film_id)
+            await self.redis.save_by_key(key, film)
         return Film(**film['_source']) if film else None
 
     async def get(self, sort: str, genre_id: str | None = None, page_number: int | None = 1, page_size: int | None = 10) -> list[Film]:
         key = make_redis_key('films', sort, genre_id, page_number, page_size)
-        films = await self._get_films_from_cache(key)
+        films = await self.redis.get_by_key(key)
         if not films:
             films = await self._get_films_from_elastic(sort=sort, genre_id=genre_id, page_number=page_number, page_size=page_size)
-            await self._save_films_to_cache(key, films)
+            await self.redis.save_by_key(key, films)
         return [Film(**doc['_source']) for doc in films['hits']['hits']]
 
     async def search(self, query: str, page_number: int | None = 1, page_size: int | None = 10) -> list[Film]:
         key = make_redis_key('films', query, page_number, page_size)
-        films = await self._get_films_from_cache(key)
+        films = await self.redis.get_by_key(key)
         if not films:
             films = await self._search_films_in_elastic(query, page_number=page_number, page_size=page_size)
-            await self._save_films_to_cache(key, films)
+            await self.redis.save_by_key(key, films)
         return [Film(**doc['_source']) for doc in films['hits']['hits']]
-    
-    async def _get_films_from_cache(self, key) -> dict | list[dict]:
-        films = await self.redis.get(key)
-        return orjson.loads(films) if films else None
-        
-    async def _save_films_to_cache(self, key, films):
-        await self.redis.set(key, orjson.dumps(films).decode(), CACHE_EXPIRE_IN_SECONDS)
-
-    async def _get_film_from_elastic(self, film_id: str) -> dict | None:
-        try:
-            return await self.elastic.get('movies', film_id)
-        except NotFoundError:
-            return None
 
     async def _get_films_from_elastic(
         self, sort: str, genre_id: str | None = None, page_number: int | None = 1, page_size: int | None = 10
@@ -62,9 +48,8 @@ class FilmService(ServiceAbstract):
 
         body = {
             'sort': [{sort_field: sort_order}],
-            'from': (page_number - 1) * page_size,
-            'size': page_size,
         }
+
         if genre_id:
             body |= {
                 'query': {
@@ -75,10 +60,10 @@ class FilmService(ServiceAbstract):
                 },
             }
             
-        return await self.elastic.search(index='movies', body=body)
+        return await self.elastic.search_by_query_with_pagination(index='movies', body=body, page_number=page_number, page_size=page_size)
 
     async def _search_films_in_elastic(self, query: str, page_number: int | None = 1, page_size: int | None = 10) -> dict:
-        return await self.elastic.search(
+        return await self.elastic.search_by_query_with_pagination(
             index='movies',
             body={
                 'query': {
@@ -94,15 +79,15 @@ class FilmService(ServiceAbstract):
                         ],
                     }
                 },
-                'from': (page_number - 1) * page_size,
-                'size': page_size,
             },
+            page_number=page_number,
+            page_size=page_size,
         )
 
 
 @lru_cache()
 def get_film_service(
-    redis: Redis = Depends(get_redis),
-    elastic: AsyncElasticsearch = Depends(get_elastic),
+    redis: RedisClient = Depends(get_redis),
+    elastic: ElasticClient = Depends(get_elastic),
 ) -> FilmService:
     return FilmService(redis, elastic)
--- movies_sp5/movies_app/src/services/person.py
+++ movies_sp5/movies_app/src/services/person.py
@@ -1,56 +1,42 @@
 from functools import lru_cache
 
-import orjson
-from elasticsearch import AsyncElasticsearch, NotFoundError
 from fastapi import Depends
-from redis.asyncio import Redis
 
 from db.elastic import get_elastic
 from db.redis import get_redis
 from models.person import Person, PersonFilmDetails
-from services.common import make_redis_key, CACHE_EXPIRE_IN_SECONDS, ServiceAbstract
+from db.elastic import ElasticClient
+from db.redis import RedisClient
+from services.common import make_redis_key, ServiceAbstract
 
 
 class PersonService(ServiceAbstract):
     async def get_by_id(self, person_id: str) -> Person | None:
         key = make_redis_key('person', person_id)
-        person = await self._get_persons_from_cache(key)
+        person = await self.redis.get_by_key(key)
         if not person:
-            person = await self._get_person_from_elastic(person_id)
-            await self._save_persons_to_cache(key, person)
+            person = await self.elastic.get_by_id('persons', person_id)
+            await self.redis.save_by_key(key, person)
         return Person(**person['_source']) if person else None
     
     async def get_films_by_person_id(self, person_id: str, page_number: int | None = 1, page_size: int | None = 10) -> list[Person]:
         key = make_redis_key('person_films', person_id)
-        person_films = await self._get_persons_from_cache(key)
+        person_films = await self.redis.get_by_key(key)
         if not person_films:
             person_films = await self._search_person_films_in_elastic(person_id, page_number=page_number, page_size=page_size)
-            await self._save_persons_to_cache(key, person_films)
+            await self.redis.save_by_key(key, person_films)
         return [PersonFilmDetails(**doc['_source']) for doc in person_films['hits']['hits']]
     
     async def search(self, query: str, page_number: int | None = 1, page_size: int | None = 10) -> list[Person]:
         key = make_redis_key('persons', query, page_number, page_size)
-        persons = await self._get_persons_from_cache(key)
+        persons = await self.redis.get_by_key(key)
         if not persons:
             persons = await self._search_persons_in_elastic(query, page_number=page_number, page_size=page_size)
-            await self._save_persons_to_cache(key, persons)
+            await self.redis.save_by_key(key, persons)
         return [Person(**doc['_source']) for doc in persons['hits']['hits']]
 
-    async def _get_persons_from_cache(self, key) -> dict | list[dict]:
-        persons = await self.redis.get(key)
-        return orjson.loads(persons) if persons else None
-        
-    async def _save_persons_to_cache(self, key, persons):
-        await self.redis.set(key, orjson.dumps(persons).decode(), CACHE_EXPIRE_IN_SECONDS)
-
-    async def _get_person_from_elastic(self, person_id: str) -> dict | None:
-        try:
-            return await self.elastic.get('persons', person_id)
-        except NotFoundError:
-            return None
-
     async def _search_person_films_in_elastic(self, person_id, page_number: int | None = 1, page_size: int | None = 10) -> dict:
-        return await self.elastic.search(
+        return await self.elastic.search_by_query_with_pagination(
             index='movies',
             body={
                 'query': {
@@ -80,13 +66,13 @@ class PersonService(ServiceAbstract):
                         ]
                     }
                 },
-                'from': (page_number - 1) * page_size,
-                'size': page_size,
             },
+            page_number=page_number,
+            page_size=page_size,
         )
 
     async def _search_persons_in_elastic(self, query: str, page_number: int | None = 1, page_size: int | None = 10) -> dict:
-        return await self.elastic.search(
+        return await self.elastic.search_by_query_with_pagination(
             index='persons',
             body={
                 'query': {
@@ -94,15 +80,15 @@ class PersonService(ServiceAbstract):
                         'full_name': query
                     }
                 },
-                'from': (page_number - 1) * page_size,
-                'size': page_size,
             },
+            page_number=page_number,
+            page_size=page_size,
         )
 
 
 @lru_cache()
 def get_person_service(
-        redis: Redis = Depends(get_redis),
-        elastic: AsyncElasticsearch = Depends(get_elastic),
+        redis: RedisClient = Depends(get_redis),
+        elastic: ElasticClient = Depends(get_elastic),
 ) -> PersonService:
     return PersonService(redis, elastic)
