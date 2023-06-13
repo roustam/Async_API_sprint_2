0a865594aa9d1148a7f210706f34f9921203ff62
Roustam Dzhafarov <roustam.ts@gmail.com>
2023-06-13 16:31:51 +0400

    very first commit

--- movies_sp5/tests/functional/utils/wait_for_redis.py
+++ movies_sp5/tests/functional/utils/wait_for_redis.py
@@ -1,15 +1,27 @@
 import asyncio
-
+import logging
 from redis.asyncio import Redis
+import traceback
 
 from settings import redis_settings
 
-
-async def wait_redis(redis_client: Redis):
+async def wait_redis(redis_client: Redis, 
+                     start_sleep_time=0.1, factor=2, border_sleep_time=10.0):
+    n = 0
+    t = start_sleep_time
     while True:
-        if await redis_client.ping():
-            break
-        await asyncio.sleep(1)
+        try:
+            if await redis_client.ping():
+                break
+            elif t == border_sleep_time:
+                logging.error(f'Border time exceeded after {n} attempts')
+                break
+            else:
+                t = start_sleep_time * (factor**n) if t < border_sleep_time else border_sleep_time
+                await asyncio.sleep(t)
+        except Exception as err:
+            logging.error(err)
+            logging.error(traceback.format_exc())
 
 
 if __name__ == '__main__':
